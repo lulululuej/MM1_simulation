1dee6e769f46c669b9665060507cbfbf894c1d30
karenchow <j57078222@gmail.com>
2021-08-27 14:39:53 +0800

    float -> double

--- MM1.c
+++ MM1.c
@@ -6,9 +6,9 @@
 #define BUSY 1
 #define IDLE 0
 #define Q_LIMIT 1000000
-
-int num_delays_required, next_event_type, num_cus_delayed, num_events, num_in_q, server_status;
-float mean_inter_arr_time, mean_service_time, area_num_in_q, area_server_status, sim_time, time_arrival[Q_LIMIT + 1], time_last_event, next_arrival_time, next_departure_time, total_of_delays;
+int next_event_type, num_events, server_status, num_in_q;
+int num_delays_required, num_cus_delayed;
+long double mean_inter_arr_time, mean_service_time, area_num_in_q, area_server_status, sim_time, time_arrival[Q_LIMIT + 1], time_last_event, next_arrival_time, next_departure_time, total_of_delays;
 FILE *infile, *outfile;
 
 void initialize(void);
@@ -17,19 +17,17 @@ void update_time_avg_stats(void);
 void arrival(void);
 void departure(void);
 void report(void);
-float exponential(float mean);
+double exponential(double mean);
 
 int main() {
     infile = fopen("mm1.in", "r");
     outfile = fopen("mm1.out", "w");
     num_events = 2; // arrive 1 and depart 2
     
-    fscanf(infile, "%d %f %f", &num_delays_required, &mean_inter_arr_time, &mean_service_time);
-    int a = 5;
-    //printf("num of delay required = %d\n", num_delays_required);
-    //printf("mean interval = %f\n", mean_interval);
-    //printf("mean service = %f\n", mean_service);
-    //printf(num_delays_required,mean_service,mean_interval);
+    fscanf(infile, "%d %Lf %Lf", &num_delays_required, &mean_inter_arr_time, &mean_service_time);
+    printf("num of delay required = %d\n", num_delays_required);
+    printf("mean interval = %Lf\n", mean_inter_arr_time);
+    printf("mean service = %Lf\n", mean_service_time);
     initialize();
     
     while(num_cus_delayed < num_delays_required){
@@ -73,7 +71,7 @@ void initialize(void){
 }
 
 void timing(void){
-    float min_time_next_event;
+    double min_time_next_event;
     next_event_type = 0;
     //printf("next arrival time = %f next departure time = %f\n",next_arrival_time,next_departure_time);
     //compare and choose the smallest time to occur
@@ -88,7 +86,7 @@ void timing(void){
     //printf("next event type = %d", next_event_type);
 
     if(next_event_type == 0){
-        fprintf(outfile,"\nEvent list is empty at time %f",sim_time);
+        fprintf(outfile,"\nEvent list is empty at time %Lf",sim_time);
         exit(1);
     }
     
@@ -96,22 +94,24 @@ void timing(void){
     //printf("sim time = %f\n" ,sim_time);
 }
 
-float exponential(float mean){
+double exponential(double mean){
     return -mean * log(lcgrand(1));
 }
 
 void update_time_avg_stats(void){
-    float time_since_last_event;
+    double time_since_last_event;
     time_since_last_event = sim_time - time_last_event;
+    //printf("simulation time = %lf\n", sim_time);
+    //printf("time last event = %lf\n", time_last_event);
+    //printf("time since last event = %lf\n", time_since_last_event);
     time_last_event = sim_time;
     
     area_num_in_q += num_in_q * time_since_last_event;
     area_server_status += server_status * time_since_last_event;
-    //printf("area_num_in_q = %f\n", area_num_in_q);
 }
 
 void arrival(void){
-    float delay;
+    double delay;
     //printf("server status = %d\n", server_status);
     
     next_arrival_time = sim_time + exponential(mean_inter_arr_time);
@@ -119,7 +119,7 @@ void arrival(void){
         ++num_in_q;
         if (num_in_q > Q_LIMIT){
             fprintf(outfile, "\nOverflow of the array time_arrival at");
-            fprintf(outfile, " time %f", sim_time);
+            fprintf(outfile, " time %Lf", sim_time);
             exit(2);
         }
         time_arrival[num_in_q] = sim_time;
@@ -135,7 +135,8 @@ void arrival(void){
 }
 
 void departure(void){
-    float delay;
+    //++num_cus_delayed;
+    double delay;
     if(num_in_q == 0){
         server_status = IDLE;
         next_departure_time = 1.0e+30;
@@ -154,22 +155,22 @@ void departure(void){
 }
 
 void report(void){
-    float mean_inter_arr_rate, mean_service_rate, theo_avg_delay, theo_avg_num_in_q, theo_server_util;
+    long double mean_inter_arr_rate, mean_service_rate, theo_avg_delay, theo_avg_num_in_q, theo_server_util;
     mean_inter_arr_rate = pow(mean_inter_arr_time,-1);
     mean_service_rate = pow(mean_service_time,-1);
     theo_avg_delay = mean_inter_arr_rate/mean_service_rate/(mean_service_rate-mean_inter_arr_rate);
     theo_avg_num_in_q = pow(mean_inter_arr_rate,2)/(mean_service_rate*(mean_service_rate-mean_inter_arr_rate));
     theo_server_util = mean_inter_arr_rate/mean_service_rate;
-    fprintf(outfile, "Mean interarrival time%11.3f minutes\n", mean_inter_arr_time);
-    fprintf(outfile, "Mean service time%16.3f minutes\n", mean_service_time);
+    fprintf(outfile, "Mean interarrival time%11.3Lf minutes\n", mean_inter_arr_time);
+    fprintf(outfile, "Mean service time%16.3Lf minutes\n", mean_service_time);
     fprintf(outfile, "Number of customers%16.3d\n", num_delays_required);
     fprintf(outfile, "-----------------------------------------\n");
-    fprintf(outfile, "[Theo] Avg delay in queue%8.3f minutes\n",theo_avg_delay);
-    fprintf(outfile, "[Expe] Avg delay in queue%8.3f minutes\n\n",total_of_delays/num_cus_delayed);
-    fprintf(outfile, "[Theo] Avg number in queue%7.3f \n",theo_avg_num_in_q);
-    fprintf(outfile, "[Expe] Avg number in queue%7.3f \n\n",area_num_in_q/sim_time);
-    fprintf(outfile, "[Theo] Server utilization%8.3f \n",theo_server_util);
-    fprintf(outfile, "[Expe] Server utilization%8.3f \n",area_server_status/sim_time);
+    fprintf(outfile, "[Theo] Avg delay in queue%8.3Lf minutes\n",theo_avg_delay);
+    fprintf(outfile, "[Expe] Avg delay in queue%8.3Lf minutes\n\n",total_of_delays/num_cus_delayed);
+    fprintf(outfile, "[Theo] Avg number in queue%7.3Lf \n",theo_avg_num_in_q);
+    fprintf(outfile, "[Expe] Avg number in queue%7.3Lf \n\n",area_num_in_q/sim_time);
+    fprintf(outfile, "[Theo] Server utilization%8.3Lf \n",theo_server_util);
+    fprintf(outfile, "[Expe] Server utilization%8.3Lf \n",area_server_status/sim_time);
     fprintf(outfile, "-----------------------------------------\n");
-    fprintf(outfile, "Time simulation ended%12.3f minutes", sim_time);
+    fprintf(outfile, "Time simulation ended%12.3Lf minutes", sim_time);
 }
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    double a = 1;
+    printf("a = %lf",a);
+}
